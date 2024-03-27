b634e68cdb659c7f468ee3c214d0908c9df51f44
Goral Patel <105250604+GxPatel@users.noreply.github.com>
2024-03-27 15:02:32 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -183,11 +183,3 @@ text-align: center
 </div>
 """,
 unsafe_allow_html=True)
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
